6c09516b029b812f04bc6d2c725d73bda865de3d
ducthang321 <119120431+ducthang321@users.noreply.github.com>
2024-10-23 11:00:02 +0700

    Update lsu.sv

--- lsu.sv
+++ lsu.sv
@@ -22,17 +22,17 @@ module lsu(
 	output logic [31:0] o_io_lcd
 );
 	// ------------memory
-	parameter logic [31:0] ins_memory [2047:0];
-	parameter logic [31:0] data_memory [2047:0];
+	logic [31:0] instruction_memory [2047:0];
+	logic [31:0] data_memory [2047:0];
 	
 	//------------led
-	parameter logic [31:0] led_r;
-	parameter logic [31:0] led_g;
-	parameter logic [6:0] hex7seg [7:0];
+	logic [31:0] led_r;
+	logic [31:0] led_g;
+	logic [6:0] hex7seg [7:0];
 	
-	parameter logic [31:0] lcd_display;
+	logic [31:0] lcd_display;
 	
-	always_ff @(posedge i_clk posedge i_rst) begin
+	always_ff @(posedge i_clk or posedge i_rst) begin
 		if (i_rst) begin
 		led_r <= 32'd0;
 		led_g <= 32'd0;
@@ -65,12 +65,12 @@ module lsu(
 					
 					32'h2000: data_memory[i_lsu_addr[12:2]] <= i_st_data; //*note 
 					
-					32'h7800: 	// switch
-					32'h7810:	//buttons
+					//32'h7800: 	// switch
+					//32'h7810:	//buttons
 					
 					default: if (i_lsu_addr >= 32'h0000 && i_lsu_addr <= 32'h1FFF)
                                 instruction_memory[i_lsu_addr[12:2]] <= i_st_data; // note
-					
+					endcase
 			end
 		end
 	
@@ -79,7 +79,38 @@ module lsu(
 // Doc du lieu
 	always_comb begin
 		case(i_lsu_addr)
+		32'h7000: o_ld_data = led_r;
+		32'h7010: o_ld_data = led_g;
 		
+		32'h7020: o_ld_data = {24'd0, hex7seg[0]};
+		32'h7021: o_ld_data = {24'd0, hex7seg[1]};
+		32'h7022: o_ld_data = {24'd0, hex7seg[2]};
+		32'h7023: o_ld_data = {24'd0, hex7seg[3]};
+		32'h7024: o_ld_data = {24'd0, hex7seg[4]};
+		32'h7025: o_ld_data = {24'd0, hex7seg[5]};
+		32'h7026: o_ld_data = {24'd0, hex7seg[6]};
+		32'h7027: o_ld_data = {24'd0, hex7seg[7]};
+		
+		32'h7030: o_ld_data = lcd_display;
+		
+		32'h7800: o_ld_data = i_io_sw;
+		32'h7810: o_ld_data = {28'd0, i_io_btn};
+		
+		32'h2000: o_ld_data = data_memory[i_lsu_addr[12:2]]; //note
+		default: o_ld_data = instruction_memory[i_lsu_addr[12:2]]; //note
+		endcase
 	end
+	
+	assign o_io_ledr = led_r;
+	assign o_io_ledg = led_g;
+	assign o_io_hex0 = hex7seg[0];
+	assign o_io_hex1 = hex7seg[1];
+	assign o_io_hex2 = hex7seg[2];
+	assign o_io_hex3 = hex7seg[3];
+	assign o_io_hex4 = hex7seg[4];
+	assign o_io_hex5 = hex7seg[5];
+	assign o_io_hex6 = hex7seg[6];
+	assign o_io_hex7 = hex7seg[7];
+	assign o_io_lcd = lcd_display;
 	
 endmodule 
